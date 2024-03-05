557931a2ba99ac441eb5615d7b943a4723330b09
Yogihafidh <hafidhyogi@gmail.com>
2024-03-05 22:29:44 +0700

    finishing UI  \

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const goals = [
       "https://images.unsplash.com/photo-1682687219800-bba120d709c5?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8",
     task: 0,
     description: "Belajar fotografi untuk dapat kerja",
-    milestone: [],
+    milestone: ["Belajar ISO", "Membeli camera"],
   },
   {
     id: 2,
@@ -15,6 +15,7 @@ const goals = [
       "https://images.unsplash.com/photo-1592609931095-54a2168ae893?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bm9kZSUyMGpzfGVufDB8fDB8fHww",
     task: 0,
     description: "Belajar Node.jS untuk dapat membangun aplikasi backend",
+    milestone: ["Mempelajari javascript", "Membuat API"],
   },
   {
     id: 3,
@@ -23,12 +24,13 @@ const goals = [
       "https://images.unsplash.com/photo-1556911261-6bd341186b2f?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGNvb2tpbmd8ZW58MHx8MHx8fDA%3D",
     task: 0,
     description: "Belajar masak untuk dapat memasakan istri",
+    milestone: ["Mempelajari javascript", "Membuat API"],
   },
 ];
 
-function Button({ style, onClick, children }) {
+function Button({ className, onClick, children }) {
   return (
-    <button className={style} onClick={onClick}>
+    <button className={className} onClick={onClick}>
       {children}
     </button>
   );
@@ -38,10 +40,13 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Stats />
       <div className="goal-section">
         <GoalList goals={goals} />
       </div>
+      <GoalView />
       <FormAddGoal />
+      <FormAddTask />
     </div>
   );
 }
@@ -50,7 +55,7 @@ function Header() {
     <header>
       <p>Good evening, Yono!</p>
       <img src="./logo.png" alt="Logo" />
-      <Button style={"buttonNav"}>
+      <Button className={"buttonNav"}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
@@ -67,6 +72,15 @@ function Header() {
         <p>Add Goal</p>
       </Button>
     </header>
+  );
+}
+
+function Stats() {
+  return (
+    <div className="stats">
+      <h2>Your Goal</h2>
+      <p>You have X goal, and you already goal X (X%)</p>
+    </div>
   );
 }
 
@@ -89,20 +103,52 @@ function Goal({ goal }) {
       <div className="goal-content">
         <h3>{goal.name}</h3>
         <p>{goal.description}</p>
-        <Button style={"button"}>Open</Button>
+        <Button className={"button"}>Open</Button>
       </div>
     </li>
   );
 }
 
-function Task() {}
+function GoalView() {
+  return (
+    <>
+      <div className="overlay hidden"></div>
+      <div className="task hidden">
+        <img
+          src={
+            "https://images.unsplash.com/photo-1707343848655-a196bfe88861?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHx8"
+          }
+          alt={"hjhjh"}
+        />
+
+        <div className="task-content">
+          <h2>Nama Goal</h2>
+          <p>Description</p>
+          <ul>
+            <Milestone milestone={"Step 1"} />
+            <Milestone milestone={"Step 1"} />
+            <Milestone milestone={"Step 1"} />
+            <Milestone milestone={"Step 1"} />
+          </ul>
+          <Button className={"closeButton"}>X</Button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function Milestone({ milestone }) {
+  return <li>{milestone}</li>;
+}
 
 function FormAddGoal() {
   return (
     <>
-      <div className="overlay"></div>
-      <form className="form-add-goal">
+      <div className="overlay hidden"></div>
+      <form className="form-add-goal hidden">
         <h2>Add Goal</h2>
+
+        <Button className={"closeButton"}>X</Button>
 
         <label>Goal</label>
         <input type="text" />
@@ -112,11 +158,41 @@ function FormAddGoal() {
 
         <label>Description</label>
         <input type="text" />
+
+        <label>Milestone</label>
+        <input type="text" />
+
+        <Button className={"button"}>Add</Button>
       </form>
     </>
   );
 }
 
-function FormAddTask() {}
+function FormAddTask() {
+  return (
+    <>
+      <div className="overlay hidden"></div>
+      <form className="form-add-goal hidden ">
+        <h2>Add Goal</h2>
+
+        <Button className={"closeButton"}>X</Button>
+
+        <label>Goal</label>
+        <input type="text" />
+
+        <label>Image</label>
+        <input type="text" />
+
+        <label>Description</label>
+        <input type="text" />
+
+        <label>Milestone</label>
+        <input type="text" />
+
+        <Button className={"button"}>Add</Button>
+      </form>
+    </>
+  );
+}
 
 export default App;
